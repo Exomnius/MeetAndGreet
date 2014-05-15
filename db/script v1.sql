a29c69bdb87ec0c36419265b8a4bef5699d285a0
Asendo <nickthoelen@gmail.com>
2014-05-15 22:03:12 +0200

    New users / Fields

--- db/script v1.sql
+++ db/script v1.sql
@@ -48,17 +48,30 @@ CREATE TABLE IF NOT EXISTS `tbl_userTypes` (
 	PRIMARY KEY(`typeId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
+CREATE TABLE IF NOT EXISTS `tbl_participants` (
+	`eventId` int(5) NOT NULL,
+	`userId` int(11) NOT NULL,
+	PRIMARY KEY (`eventId`, `userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
 INSERT INTO `tbl_userTypes`(`type`) VALUES
 ('temporary'),
 ('registered');
 
 INSERT INTO `tbl_users` (`userId`, `username`, `firstname`, `lastname`, `email`, `password`, `address`, `city`, `dob`, `gender`, `member_since`, `userType`) VALUES
 (1, 'PereGullis', 'Pere', 'Gullis', 'notthevictim@gmail.com', '8c2da486a8b3e78cd123358c4be00c4d', 'Omstraat 34 bus 12', 'Bilzen', '2014-02-06', '0', '2014-02-06 14:41:48',1),
-(2, 'GullisDumfan', 'Gullis', 'Idonknow', 'adsdasdas@hotmail.com', '8c2da486a8b3e78cd123358c4be00c4d', 'Omstraat 34 bus 12', 'Bilzen', '2014-02-12', '0', '2014-02-12 22:42:12', 2);
+(2, 'GullisDumfan', 'Gullis', 'Idonknow', 'adsdasdas@hotmail.com', '8c2da486a8b3e78cd123358c4be00c4d', 'Omstraat 34 bus 12', 'Bilzen', '2014-02-12', '0', '2014-02-12 22:42:12', 1),
+(3, 'CornelJanssen', 'Cornel', 'Janssen', 'iets@iets.iets', '402bebba1c63fd382b68ee2f148586a1', 'Omstraat 34 bus 12', 'Bilzen', '2014-02-12', '0', '2014-02-12 22:42:12', 1),
+(4, 'NickThoelen', 'Cornel', 'Janssen', 'iets@iets.iets', '402bebba1c63fd382b68ee2f148586a1', 'Omstraat 34 bus 12', 'Bilzen', '2014-02-12', '0', '2014-02-12 22:42:12', 1),
+(5, 'arno', 'Cornel', 'Janssen', 'iets@iets.iets', '402bebba1c63fd382b68ee2f148586a1', 'Omstraat 34 bus 12', 'Bilzen', '2014-02-12', '0', '2014-02-12 22:42:12', 1);
 
 INSERT INTO `tbl_eventCategories` (`catId`, `categorie`, `iconURL`) VALUES
-(1, 'temporary', 'c'),
-(2, 'registered', 'd');
+(1, 'Other', 'd'),
+(2, 'Food', 'd'),
+(3, 'Drinks', 'd'),
+(4, 'Sports', 'd'),
+(5, 'Party', 'd'),
+(6, 'Socialize', 'd');
 
 INSERT INTO `tbl_events` (`user`, `description`, `catId`, `latitude`, `longitude`) VALUES
 (1, 'hackathon', 1, 51.059257, 3.720036);
@@ -71,4 +84,6 @@ ALTER TABLE `tbl_events`
 ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`catId`) REFERENCES `tbl_eventCategories` (`catId`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `events_ibfk_2` FOREIGN KEY (`catId`) REFERENCES `tbl_eventCategories` (`catId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-
+ALTER TABLE `tbl_participants`
+ADD CONSTRAINT `participants_ibfk_1` FOREIGN KEY(`eventId`) REFERENCES `tbl_events` (`eventId`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `participants_ibfk_2` FOREIGN KEY(`userId`) REFERENCES `tbl_users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
